782eb93d77e1ca1db1ec43935ea379ce49fe26e1
kensylee95 <kingsleyumenze@gmail.com>
2024-11-10 18:37:16 +0100

    update

--- src/utils/cookieOptions.ts
+++ src/utils/cookieOptions.ts
@@ -3,7 +3,7 @@ import { CookieOptions } from "express";
 export const cookieOptions:CookieOptions={
     httpOnly: true, // Ensures the cookie can't be accessed via JavaScript
     secure: true, // Ensures the cookie is sent over HTTPS
-    sameSite: 'none', // Allows the cookie to be sent in cross-origin requests
+    sameSite: 'strict', // Allows the cookie to be sent in cross-origin requests
     path:"/",
     maxAge: 24 * 60 * 60 * 1000, // 1 day expiration
 }
